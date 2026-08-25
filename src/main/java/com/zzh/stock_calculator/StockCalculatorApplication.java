518f11bed405bac67284b560683e253e1a07d8e1
qq166 <qq1663867295@outlook.com>
2026-08-25 16:46:44 +0800

    去掉webflux

--- src/main/java/com/zzh/stock_calculator/StockCalculatorApplication.java
+++ src/main/java/com/zzh/stock_calculator/StockCalculatorApplication.java
@@ -2,10 +2,13 @@ package com.zzh.stock_calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.http.client.autoconfigure.reactive.ReactiveHttpClientAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        ReactiveHttpClientAutoConfiguration.class
+})
 public class StockCalculatorApplication {
 
 	public static void main(String[] args) {
